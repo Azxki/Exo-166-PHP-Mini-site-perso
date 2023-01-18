5f5775cb423f8db5a59ebc4d0532ce697b638329
azxki <cococoursan@gmail.com>
2023-01-18 09:26:23 +0100

    commit

--- lib/functions.php
+++ lib/functions.php
@@ -8,15 +8,15 @@ la logique pour choisir la page à charger
 
 function getContent() {
 	if(!isset($_GET['page'])){
-		include __DIR__.'/../pages/home.php';
+		include dirname(__DIR__, 1).'/pages/home.php';
 	}
 	elseif(isset($_GET['page']) && $_GET['page'] == "bio") {
 
-        include __DIR__.'/../pages/bio.php';
+        include dirname(__DIR__, 1).'/pages/bio.php';
     }
 	elseif(isset($_GET['page']) && $_GET['page'] == "contact") {
 
-        include __DIR__.'/../pages/contact.php';
+        include dirname(__DIR__, 1).'/pages/contact.php';
     }
 }
 
